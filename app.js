b964099e269c2d4938ad2eb5a4c58f36cf8de752
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-09-22 16:24:19 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express") ;
 const cors = require ("cors") ;
 const app = express () ;
 const contactsRouter = require("./app/routes/contact.route");
-
+const ApiError = require("./app/api-error");
 app.use(cors());
 app.use(express.json ( ) ) ;
 app.get("/", (req,res) => {
@@ -11,4 +11,19 @@ app.get("/", (req,res) => {
 
 
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    
+         return next(new ApiError(404, "Resource not found"));
+    });
+app.use((error, req, res, next) => {
+
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
+
+
 module.exports = app;
